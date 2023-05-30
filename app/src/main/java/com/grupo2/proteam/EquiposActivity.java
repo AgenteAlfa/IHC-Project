7c5a7d86ff06578ad9f0c9acbb8baae33e7f7792
Arnold <arnoldjbs@gmail.com>
2023-05-30 03:02:24 -0500

    Agregando registro de datos 2

--- app/src/main/java/com/grupo2/proteam/EquiposActivity.java
+++ app/src/main/java/com/grupo2/proteam/EquiposActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,34 +16,33 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.grupo2.proteam.databinding.ActivityEquiposBinding;
-
 public class EquiposActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityEquiposBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equipos);
+        View AppBar = findViewById(R.id.Equipos_AppBar);
+        Toolbar tbar = AppBar.findViewById(R.id.abEquipos_toolbar);
+        setSupportActionBar(tbar);
 
-        binding = ActivityEquiposBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.Equipos_nvLateral);
 
-        setSupportActionBar(binding.appBarEquipos.toolbar);
-
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.navEquipos_home, R.id.navEquipos_gallery, R.id.navEquipos_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_equipos);
+
+        NavController navController = Navigation.findNavController(this, R.id.contenedorEquipos_frgContenedor);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        binding.appBarEquipos.fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton FabAyuda = AppBar.findViewById(R.id.appbar_fabAyuda);
+        FabAyuda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -59,7 +60,7 @@ public class EquiposActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_equipos);
+        NavController navController = Navigation.findNavController(this, R.id.contenedorEquipos_frgContenedor);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
